c3c982a890e674e8fe02716b189d1ceda3c621e9
Bplest00 <92437530+Bplest00@users.noreply.github.com>
2022-01-18 17:22:19 +0100

    menu
    menu

--- str_zad3.c
+++ str_zad3.c
@@ -27,9 +27,6 @@ Pozicija StvoriOsobu(char* ime, char* prezime, int godrod) { // OVA FUNKCIJA SAM
 
 	return nova_adresa;
 }
-
-
-
 Pozicija DodajNaPocetak(Pozicija head, char* ime, char* prezime, int godrod) { //Jeli ovo mogla biti void funkcija?
 	Pozicija nova_adresa = NULL;
 	nova_adresa = StvoriOsobu(ime, prezime, godrod);  //sada imamo adresu nove osobe --> potrebno je unit vrijednosti i povezat s headom
@@ -38,37 +35,12 @@ Pozicija DodajNaPocetak(Pozicija head, char* ime, char* prezime, int godrod) { /
 }
 
 
-
-Pozicija DodajIspred(Pozicija head, Pozicija misto, char* ime, char* prezime, int godrod) {
-	Pozicija temp = head -> next;
-	Pozicija nova_adresa = NULL;
-	Pozicija veza=NULL;
-	while (temp != NULL || temp->next != misto) {
-		temp = temp->next;
-	}
-	if (temp->next == misto) {
-		veza = (temp->next)->next;
-		nova_adresa = StvoriOsobu(ime, prezime, godrod);
-		temp->next = nova_adresa;
-		nova_adresa->next = veza;
-
-	}
-	else {
-		printf("Trazeno mjesto ne postoji");
-		return NULL;
-	}
-
-}
-
-
-
 Pozicija DodajIza(Pozicija pozicija, char* ime, char* prezime, int godrod) { //Isto pitanje kao za proslu funkciju, s obzirom da smo "call by reference" zar funkcija nebi trebalo samo prominit adrese
 	Pozicija nova_adresa = NULL;
 	nova_adresa = StvoriOsobu(ime, prezime, godrod);
 	nova_adresa->next = pozicija->next;
 	pozicija->next = nova_adresa;
 }
-
 
 Pozicija DodajNaKraj(Pozicija head, char* ime, char* prezime, int godrod) { //head jer lista moze bit prazna
 	Pozicija temp = head;
@@ -81,9 +53,6 @@ Pozicija DodajNaKraj(Pozicija head, char* ime, char* prezime, int godrod) { //he
 	}
 	temp->next = nova_adresa;
 }
-
-
-
 Pozicija IzbrisiNakon(Pozicija head, Pozicija misto) { //Nakon misto brisemo sljedeceg
 	Pozicija temp = head->next;
 	Pozicija veza = NULL;
@@ -93,8 +62,6 @@ Pozicija IzbrisiNakon(Pozicija head, Pozicija misto) { //Nakon misto brisemo slj
 	temp->next = (misto->next)->next;
 	free(misto->next);
 }
-
-
 Pozicija IzbrisiPoPoziciji(Pozicija head, int n) { // koristit cu head jer ce mi tad N-ta pozicija bit ona koju brisen, ne N-1
 	int i;
 	Pozicija misto = head->next;
@@ -118,18 +85,75 @@ Pozicija IzbrisiPoPoziciji(Pozicija head, int n) { // koristit cu head jer ce mi
 
 }
 
+Pozicija DodajIspred(Pozicija head, Pozicija misto, char* ime, char* prezime, int godrod) {
+	Pozicija temp = head->next;
+	Pozicija nova_adresa = NULL;
+	Pozicija veza = NULL;
+	while (temp != NULL || temp->next != misto) {
+		temp = temp->next;
+	}
+	if (temp->next == misto) {
+		veza = (temp->next)->next;
+		nova_adresa = StvoriOsobu(ime, prezime, godrod);
+		temp->next = nova_adresa;
+		nova_adresa->next = veza;
+
+	}
+	else {
+		printf("Trazeno mjesto ne postoji");
+		return NULL;
+	}
+
+}
+
 // DODAJ ISPRED + IZBRISI ISPRED + PRONADI PO PREZIMENU + UPISUJE LISTU U DATOTEKU + CITA LISTU + PRINTA LISTU
-
-
-Pozicija BrisiListu(Pozicija misto) { // ILI OD POCETKA ILI OD NEKOG ELEMENTA
-	Pozicija temp = misto;
+int BrisiListu(Pozicija head) {
+	Pozicija temp = head->next;
 	Pozicija temp2 = NULL;
-	while (temp->next != NULL) {
+	if (!temp) {
+		return EXIT_SUCCESS;
+	}
+	while (temp != NULL) {
 		temp2 = temp;
 		temp = temp->next;
 		free(temp2);
 	}
-	free(temp);
+	//if (temp != NULL) { free(temp); }
+
+}
+int PronadiPrezime(Pozicija head, char* trazenoprezime) {
+	Pozicija temp = head->next;
+	int i;
+	if (!temp)
+	{
+		printf("Lista je prazna");
+		return EXIT_SUCCESS;
+	}
+	i = 1;
+	while (temp != NULL) {
+		if (strcmp(temp->prezime, trazenoprezime) == 0)
+		{
+			printf("\nTrazeni element se nalazi na poziciji %d", i);
+			return EXIT_SUCCESS;
+		}
+		temp = temp->next;
+		i++;
+	}
+	return EXIT_SUCCESS;
+
+}
+int PrintajListu(Pozicija head) {
+	Pozicija temp = head->next;
+	if (!temp)
+	{
+		printf("\nLista je prazna");
+		return EXIT_SUCCESS;
+	}
+	while (temp != NULL) {
+		printf("%s %s %d\n", temp->ime, temp->prezime, temp->god_rod);
+		temp = temp->next;
+	}
+	return EXIT_SUCCESS;
 }
 
 int SpremiUFajl(FILE* out, Pozicija head) { // pitaj profesora kako otvorit fajl unutar funkcije, odnosno kako path mu poslat? Moze li char* 
@@ -141,47 +165,120 @@ int SpremiUFajl(FILE* out, Pozicija head) { // pitaj profesora kako otvorit fajl
 		temp = temp->next;
 	}
 
-	return 0;}
+	return 0;
+}
 
-
-int PrintajListu(Pozicija head) {
+int CitajIzFajla(FILE* in, Pozicija head){
+	if (head->next) {
+		BrisiListu(head);
+	}
 	Pozicija temp = NULL;
-	if (head->next == NULL) {
-		printf("Lista je prazna");
+	char temp_ime[MAX_BROJ];
+	char temp_prezime[MAX_BROJ];
+	int temp_godrod;
+	int status = 1;
+	int numbyte = 0;
+	int filelen = 0; 
+	char* buffer, * cbuffer;
+	fseek(in, 0, SEEK_END);
+	filelen = ftell(in);
+	buffer = (char*)malloc(filelen+1, sizeof(char));
+	if (!buffer) {
+		printf("Can't allocate memory");
 		return -1;
 	}
-	else {
-		temp = head->next;
-		while (temp != NULL) {
-			printf("%s %s %d", temp->ime, temp->prezime, temp->god_rod);
-			temp = temp->next;
+	rewind(in);
+	fread(buffer, sizeof(char), filelen + 1,in);
+	cbuffer = buffer;
+	while (strlen(cbuffer) > 0) {
+		status = sscanf("%s %s %d %n", temp_ime, temp_prezime, &temp_godrod, &numbyte);
+		if (!status) {
+			printf("Unsupported document format");
+			return -2;
 		}
+		DodajSortirano(head, temp_ime, temp_prezime, temp_godrod);
+		cbuffer = cbuffer + numbyte;
 	}
-	return 0;
+	free(buffer);
+	return EXIT_SUCCESS;
+
+
+}
+int DodajSortirano(Pozicija head, char* ime, char* prezime, int godrod) {
+	Pozicija nova_osoba = StvoriOsobu(ime, prezime, godrod);
+	Pozicija temp = head->next;
+	if (!temp) {
+		head->next = nova_osoba;
+		return EXIT_SUCCESS;
+	}
+	while (temp->next != NULL)
+		{
+		if (strcmp(prezime, temp->prezime) > 0)  //prezime>temp->prezime ---> prezime iza po abece
+			{
+			temp = temp->next;
+			}
+		else if (strcmp(prezime, temp->prezime) < 0) 
+			{
+			nova_osoba->next = temp;
+			return EXIT_SUCCESS;
+			}
+		}
+	temp->next = nova_osoba;
+	return EXIT_SUCCESS;
+	
+
 }
 
-int CitajIzFajla(FILE* in, Pozicija head)    // --> Sscanf triban bolje skuzit
+int SortirajListu(Pozicija head)
 {
-	return 0;
+	Pozicija temp0 = head;
+	Pozicija temp = head->next;
+	Pozicija temp2 = temp->next;
+	while (temp != NULL) 
+	{
+		while (temp2 != NULL) 
+		{
+			if (strcmp(temp->prezime, temp2->prezime) > 0)
+			{
+				temp2 = temp2->next;
+			}
+			if (strcmp(temp->prezime, temp2->prezime) < 0) 
+				{
+				DodajIspred(head, temp2, temp->ime, temp->prezime, temp->god_rod);
+				IzbrisiNakon(head,temp0);
+				}
+			if (temp2->next = NULL) {
+				DodajIza(temp2, temp->ime, temp->prezime, temp->god_rod);
+				IzbrisiNakon(head, temp0);
+			}
+		}
+		temp0 = temp;
+		temp = temp->next;
+		temp2 = temp->next;
+	}
+	return EXIT_SUCCESS;
 }
+
 
 
 
 int main() {
-	int provjera, index;
+	Osoba head = { .next = NULL, .ime = {0}, .prezime = {0}, .god_rod = 0 };
 	FILE* in, * out;
-	Osoba head = { .next = NULL, .ime = {0}, .prezime = {0}, .god_rod = 0 }; // definitivno prepisa s primjera, nisam dosad ovako strukturu inicializira
+	in = fopen("lista.txt", "r");
+	out = fopen("lista_ispis.txt", "w");
 	Pozicija pocetak = &head;
-	out = fopen("lista.txt", "w"); //sada netriba provjera jel otvoren fajl jer ce ga svakako stvorit
-	provjera = 0;
-	char izbor = 'a';
+	int provjera, index;
+	provjera = 1;
+	char izbor;
+	izbor = 'a';
 	char ime[MAX_BROJ];
 	char prezime[MAX_BROJ];
 	int godrod;
 	while (provjera != -1) {
 		printf("Unesite izbor:\n");
-		printf("A-dodaj element na pocetak\nB-dodaj element na kraj\nC-Brisi element na mjestu:\nD-Pohrani u FILE:\nE-Brisi sve elemente iza\nF-pronadi element po prezimenu\nE-Izadi iz programa");
-		scanf("%c", izbor);
+		printf("A-dodaj element na pocetak\nB-printaj listu\nC-dodaj na kraj\nD-pronadi po prezimenu:\nE-Brisi element na mistu:\nF-Spremi u file\nG-Citaj iz fajla\nH-Dodaj sortirano\nS-Sortiraj listu\n X-Brisi listu, izlaz\n");
+		scanf(" %c", &izbor);
 		switch (izbor) {
 		case 'A':
 			scanf(" %s %s %d", ime, prezime, &godrod);
@@ -189,27 +286,45 @@ int main() {
 			break;
 
 		case 'B':
+			PrintajListu(&head);
+			break;
+
+		case 'C':
 			scanf(" %s %s %d", ime, prezime, &godrod);
 			DodajNaKraj(pocetak, ime, prezime, godrod);
 			break;
-		case 'C':
+		case 'D':
+			scanf("%s", prezime);
+			PronadiPrezime(&head, prezime);
+
+		case 'E':
 			printf("\n Unesite koji element zelite izbrisat:\t");
 			scanf(" %d", &index);
 			IzbrisiPoPoziciji(pocetak, index);
 			break;
-		case 'D':
-			SpremiUFajl(out, pocetak);
+		case 'F':
+			SpremiUFajl(out, &head);
+			break;
+		case 'G':
+			CitajIzFajla(in, &head);
+			break;
+		case 'H':
+			scanf(" %s %s %d", ime, prezime, &godrod);
+			DodajSortirano(&head, ime, prezime, godrod);
 			break;
 
-		case 'E':
+		case 'X':
 			BrisiListu(pocetak);
 			provjera = -1;
-			return provjera; //neznam jeli se ovako moze koristit return
+			return provjera; 
 
 		default:
 			continue;
 
 		}
-	}   // DO SLJEDECIH VJEZBI POKUSAT CU DA MENU RADI 
+	}
 
-	return 0;}
+	return 0;
+
+
+
